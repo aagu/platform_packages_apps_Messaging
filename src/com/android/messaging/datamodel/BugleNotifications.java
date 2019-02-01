MoKee/android_packages_apps_Messaging

Messaging: Add delete action for captchas notification
Change-Id: Ib9a65ddc198cf1eff221c0385e254f3b68e121c9

src/com/android/messaging/datamodel/BugleNotifications.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The Android Open Source Project
- * Copyright (C) 2015-2016 The MoKee Open Source Project
+ * Copyright (C) 2015-2018 The MoKee Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,6 +49,7 @@ import android.text.style.TextAppearanceSpan;
 import com.android.messaging.Factory;
 import com.android.messaging.R;
 import com.android.messaging.datamodel.DatabaseHelper.ConversationColumns;
+import com.android.messaging.datamodel.DatabaseHelper.PartColumns;
 import com.android.messaging.datamodel.MessageNotificationState.BundledMessageNotificationState;
 import com.android.messaging.datamodel.MessageNotificationState.ConversationLineInfo;
 import com.android.messaging.datamodel.MessageNotificationState.MultiConversationNotificationState;
@@ -65,6 +66,7 @@ import com.android.messaging.datamodel.media.MessagePartVideoThumbnailRequestDes
 import com.android.messaging.datamodel.media.UriImageRequestDescriptor;
 import com.android.messaging.datamodel.media.VideoThumbnailRequest;
 import com.android.messaging.receiver.CaptchasReceiver;
+import com.android.messaging.receiver.DeleteMessageReceiver;
 import com.android.messaging.receiver.MarkAsReadReceiver;
 import com.android.messaging.sms.MmsSmsUtils;
 import com.android.messaging.sms.MmsUtils;
@@ -194,70 +196,6 @@ public class BugleNotifications {
         if ((coverage & UPDATE_ERRORS) != 0) {
             MessageNotificationState.checkFailedMessages();
         }
-    }
-
-    private static class CaptchasNotificationState extends MessageNotificationState {
-        CaptchasNotificationState() {
-            super(null);
-        }
-
-        @Override
-        protected Style build(Builder builder) {
-            return null;
-        }
-
-        @Override
-        public boolean getNotificationVibrate() {
-            return true;
-        }
-    }
-
-    public static void postCaptchasNotication(String conversationId, String captchas, String captchaProvider) {
-        cancel(PendingIntentConstants.CAPTCHAS_NOTIFICATION_ID);
-        final NotificationState state = MessageNotificationState.getNotificationState();
-        final Context context = Factory.get().getApplicationContext();
-
-        String title = TextUtils.isEmpty(captchaProvider) ? String.format(context.getString(R.string.captchas_title), captchas)
-                : String.format(context.getString(R.string.captchas_with_provider_title), captchas, captchaProvider);
-        String content = context.getString(R.string.captchas_content);
-
-        Intent pendingIntent = new Intent();
-        pendingIntent.setClass(context, CaptchasReceiver.class);
-        pendingIntent.putExtra("captchas", captchas);
-        pendingIntent.putExtra(ConversationColumns.SMS_THREAD_ID, conversationId);
-        PendingIntent captchasIntent = PendingIntent.getBroadcast(context, 0, pendingIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
-        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-        builder.setContentTitle(title)
-                .setTicker(title)
-                .setSmallIcon(R.drawable.ic_sms_light)
-                .setContentText(content)
-                .setWhen(state != null ? state.getLatestReceivedTimestamp() : System.currentTimeMillis())
-        // Returning PRIORITY_HIGH causes L to put up a HUD notification. Without it, the ticker
-        // isn't displayed.
-                .setPriority(Notification.PRIORITY_HIGH)
-                .setContentIntent(captchasIntent)
-                .setSound(state != null ? RingtoneUtil.getNotificationRingtoneUri(state.getRingtoneUri())
-                        : getNotificationRingtoneUriForConversationId(conversationId))
-                .setColor(context.getResources().getColor(R.color.notification_accent_color));
-
-        final NotificationCompat.BigTextStyle bigTextStyle =
-                new NotificationCompat.BigTextStyle(builder);
-        bigTextStyle.setBigContentTitle(title);
-        bigTextStyle.bigText(content);
-        final Notification notification = bigTextStyle.build();
-
-        final NotificationManagerCompat notificationManager =
-                NotificationManagerCompat.from(Factory.get().getApplicationContext());
-
-        int defaults = Notification.DEFAULT_LIGHTS;
-        if (BugleNotifications.shouldVibrate(new CaptchasNotificationState())) {
-            defaults |= Notification.DEFAULT_VIBRATE;
-        }
-        notification.defaults = defaults;
-        notificationManager.notify(PendingIntentConstants.CAPTCHAS_NOTIFICATION_ID, notification);
-
     }
 
     /**
@@ -1107,6 +1045,85 @@ public class BugleNotifications {
 
         LogUtil.i(TAG, "Notifying for conversation " + conversationId + "; "
                 + "tag = " + notificationTag + ", type = " + type);
+    }
+
+    private static class CaptchasNotificationState extends MessageNotificationState {
+        CaptchasNotificationState() {
+            super(null);
+        }
+
+        @Override
+        protected Style build(Builder builder) {
+            return null;
+        }
+
+        @Override
+        public boolean getNotificationVibrate() {
+            return true;
+        }
+    }
+
+    private static void addDeleteMessageAction(final NotificationCompat.Builder notifBuilder, final String messageId) {
+        final Context context = Factory.get().getApplicationContext();
+
+        Intent deleteIntent = new Intent(context, DeleteMessageReceiver.class);
+        deleteIntent.putExtra(PartColumns.MESSAGE_ID, messageId);
+        final PendingIntent deletePendingIntent = PendingIntent.getBroadcast(context, 0, deleteIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final NotificationCompat.Action.Builder actionBuilder =
+                new NotificationCompat.Action.Builder(R.drawable.ic_delete_small_light,
+                        context.getString(R.string.notification_delete), deletePendingIntent);
+        notifBuilder.addAction(actionBuilder.build());
+    }
+
+    public static synchronized void postCaptchasNotification(String conversationId, String messageId, String captchas, String captchaProvider) {
+        cancel(PendingIntentConstants.CAPTCHAS_NOTIFICATION_ID);
+        final NotificationState state = MessageNotificationState.getNotificationState();
+        final Context context = Factory.get().getApplicationContext();
+
+        String title = TextUtils.isEmpty(captchaProvider) ? String.format(context.getString(R.string.captchas_title), captchas)
+                : String.format(context.getString(R.string.captchas_with_provider_title), captchas, captchaProvider);
+        String content = context.getString(R.string.captchas_content);
+
+        Intent pendingIntent = new Intent();
+        pendingIntent.setClass(context, CaptchasReceiver.class);
+        pendingIntent.putExtra("captchas", captchas);
+        pendingIntent.putExtra(ConversationColumns.SMS_THREAD_ID, conversationId);
+        PendingIntent captchasIntent = PendingIntent.getBroadcast(context, 0, pendingIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setContentTitle(title)
+                .setTicker(title)
+                .setSmallIcon(R.drawable.ic_sms_light)
+                .setContentText(content)
+                .setWhen(state != null ? state.getLatestReceivedTimestamp() : System.currentTimeMillis())
+                // Returning PRIORITY_HIGH causes L to put up a HUD notification. Without it, the ticker
+                // isn't displayed.
+                .setPriority(Notification.PRIORITY_HIGH)
+                .setContentIntent(captchasIntent)
+                .setSound(state != null ? RingtoneUtil.getNotificationRingtoneUri(state.getRingtoneUri())
+                        : getNotificationRingtoneUriForConversationId(conversationId))
+                .setColor(context.getResources().getColor(R.color.notification_accent_color));
+
+        addDeleteMessageAction(builder, messageId);
+
+        final NotificationCompat.BigTextStyle bigTextStyle =
+                new NotificationCompat.BigTextStyle(builder);
+        bigTextStyle.setBigContentTitle(title);
+        bigTextStyle.bigText(content);
+        final Notification notification = bigTextStyle.build();
+
+        final NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(Factory.get().getApplicationContext());
+
+        int defaults = Notification.DEFAULT_LIGHTS;
+        if (BugleNotifications.shouldVibrate(new CaptchasNotificationState())) {
+            defaults |= Notification.DEFAULT_VIBRATE;
+        }
+        notification.defaults = defaults;
+        notificationManager.notify(PendingIntentConstants.CAPTCHAS_NOTIFICATION_ID, notification);
     }
 
     // This is the message string used in each line of an inboxStyle notification.

aagu/platform_packages_apps_Messaging

src/com/android/messaging/datamodel/BugleNotifications.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The Android Open Source Project
- * Copyright (C) 2015-2016 The MoKee Open Source Project
+ * Copyright (C) 2015-2018 The MoKee Open Source Project
  * Copyright (C) 2019 aagu <aaguisme@gamil.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -51,6 +51,8 @@ import android.text.style.TextAppearanceSpan;
 
 import com.android.messaging.Factory;
 import com.android.messaging.R;
+import com.android.messaging.datamodel.DatabaseHelper.ConversationColumns;
+import com.android.messaging.datamodel.DatabaseHelper.PartColumns;
 import com.android.messaging.datamodel.MessageNotificationState.BundledMessageNotificationState;
 import com.android.messaging.datamodel.MessageNotificationState.ConversationLineInfo;
 import com.android.messaging.datamodel.MessageNotificationState.MultiConversationNotificationState;
@@ -67,6 +69,7 @@ import com.android.messaging.datamodel.media.MessagePartVideoThumbnailRequestDes
 import com.android.messaging.datamodel.media.UriImageRequestDescriptor;
 import com.android.messaging.datamodel.media.VideoThumbnailRequest;
 import com.android.messaging.receiver.CaptchasReceiver;
+import com.android.messaging.receiver.DeleteMessageReceiver;
 import com.android.messaging.sms.MmsSmsUtils;
 import com.android.messaging.sms.MmsUtils;
 import com.android.messaging.ui.UIIntents;
@@ -215,11 +218,10 @@ public class BugleNotifications {
         }
     }
 
-    public static void postCaptchasNotication(String conversationId, String captchas, String captchaProvider) {
+    public static synchronized void postCaptchasNotication(String conversationId, String messageId, String captchas, String captchaProvider) {
         cancel(PendingIntentConstants.CAPTCHAS_NOTIFICATION_ID);
         final NotificationState state = MessageNotificationState.getNotificationState();
         final Context context = Factory.get().getApplicationContext();
-        final Resources resources = context.getResources();
 
         String title = TextUtils.isEmpty(captchaProvider) ? String.format(context.getString(R.string.captchas_title), captchas)
                 : String.format(context.getString(R.string.captchas_with_provider_title), captchas, captchaProvider);
@@ -245,6 +247,8 @@ public class BugleNotifications {
                 .setSound(state != null ? RingtoneUtil.getNotificationRingtoneUri(state.getRingtoneUri())
                         : getNotificationRingtoneUriForConversationId(conversationId))
                 .setColor(context.getResources().getColor(R.color.notification_accent_color));
+
+        addDeleteMessageAction(builder, messageId);
 
         final NotificationCompat.BigTextStyle bigTextStyle =
                 new NotificationCompat.BigTextStyle(builder);
@@ -1071,6 +1075,85 @@ public class BugleNotifications {
 
         LogUtil.i(TAG, "Notifying for conversation " + conversationId + "; "
                 + "tag = " + notificationTag + ", type = " + type);
+    }
+
+    private static class CaptchasNotificationState extends MessageNotificationState {
+        CaptchasNotificationState() {
+            super(null);
+        }
+
+        @Override
+        protected Style build(Builder builder) {
+            return null;
+        }
+
+        @Override
+        public boolean getNotificationVibrate() {
+            return true;
+        }
+    }
+
+    private static void addDeleteMessageAction(final NotificationCompat.Builder notifBuilder, final String messageId) {
+        final Context context = Factory.get().getApplicationContext();
+
+        Intent deleteIntent = new Intent(context, DeleteMessageReceiver.class);
+        deleteIntent.putExtra(PartColumns.MESSAGE_ID, messageId);
+        final PendingIntent deletePendingIntent = PendingIntent.getBroadcast(context, 0, deleteIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final NotificationCompat.Action.Builder actionBuilder =
+                new NotificationCompat.Action.Builder(R.drawable.ic_delete_small_light,
+                        context.getString(R.string.delete_captchas_notification), deletePendingIntent);
+        notifBuilder.addAction(actionBuilder.build());
+    }
+
+    public static synchronized void postCaptchasNotification(String conversationId, String messageId, String captchas, String captchaProvider) {
+        cancel(PendingIntentConstants.CAPTCHAS_NOTIFICATION_ID);
+        final NotificationState state = MessageNotificationState.getNotificationState();
+        final Context context = Factory.get().getApplicationContext();
+
+        String title = TextUtils.isEmpty(captchaProvider) ? String.format(context.getString(R.string.captchas_title), captchas)
+                : String.format(context.getString(R.string.captchas_with_provider_title), captchas, captchaProvider);
+        String content = context.getString(R.string.captchas_content);
+
+        Intent pendingIntent = new Intent();
+        pendingIntent.setClass(context, CaptchasReceiver.class);
+        pendingIntent.putExtra("captchas", captchas);
+        pendingIntent.putExtra(ConversationColumns.SMS_THREAD_ID, conversationId);
+        PendingIntent captchasIntent = PendingIntent.getBroadcast(context, 0, pendingIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setContentTitle(title)
+                .setTicker(title)
+                .setSmallIcon(R.drawable.ic_sms_light)
+                .setContentText(content)
+                .setWhen(state != null ? state.getLatestReceivedTimestamp() : System.currentTimeMillis())
+                // Returning PRIORITY_HIGH causes L to put up a HUD notification. Without it, the ticker
+                // isn't displayed.
+                .setPriority(Notification.PRIORITY_HIGH)
+                .setContentIntent(captchasIntent)
+                .setSound(state != null ? RingtoneUtil.getNotificationRingtoneUri(state.getRingtoneUri())
+                        : getNotificationRingtoneUriForConversationId(conversationId))
+                .setColor(context.getResources().getColor(R.color.notification_accent_color));
+
+        addDeleteMessageAction(builder, messageId);
+
+        final NotificationCompat.BigTextStyle bigTextStyle =
+                new NotificationCompat.BigTextStyle(builder);
+        bigTextStyle.setBigContentTitle(title);
+        bigTextStyle.bigText(content);
+        final Notification notification = bigTextStyle.build();
+
+        final NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(Factory.get().getApplicationContext());
+
+        int defaults = Notification.DEFAULT_LIGHTS;
+        if (BugleNotifications.shouldVibrate(new CaptchasNotificationState())) {
+            defaults |= Notification.DEFAULT_VIBRATE;
+        }
+        notification.defaults = defaults;
+        notificationManager.notify(PendingIntentConstants.CAPTCHAS_NOTIFICATION_ID, notification);
     }
 
     // This is the message string used in each line of an inboxStyle notification.